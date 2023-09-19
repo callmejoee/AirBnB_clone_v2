7ec2f0ba4ae6fa990de0a1631099febd7e15599d
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-19 20:20:22 +0300

    fix some issues update imports for relationship func. in city and user fix relationship args

--- models/city.py
+++ models/city.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, relationship
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
-    places = relationship(Place, cascade=delete, back_populates='cities')
+    places = relationship("Place", cascade="all, delete",
+                          backref='cities')
