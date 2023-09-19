abae89e3fd70a169c38ffdb2e0ad873443324c5e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-19 17:43:15 +0300

    change name to hbnb_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- script that prepares a MySQL server for the project:
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
