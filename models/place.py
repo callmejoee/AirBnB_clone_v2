400f40a2e4747ddc1e1f74a09274dc5ee5ca1b57
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-09-20 23:52:07 +0300

    pyc style fix

--- models/place.py
+++ models/place.py
@@ -11,7 +11,8 @@ from models import HBNB_TYPE_STORAGE
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
                              primary_key=True, nullable=False)
                       )
 
