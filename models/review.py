09b41868a8ab0a5ca36420d78279d5432b60a064
Youssef <youssefkaramedu@gmail.com>
2023-09-20 00:31:44 -0700

    Update Review according to task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+# Add base to the below import statement and to the review class inheritance
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id')
+    user_id = Column(String(60), ForeignKey('users.id'))
+    text = Column(String(1024), nullable=False)
