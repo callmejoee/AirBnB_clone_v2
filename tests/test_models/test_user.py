dc24c58fb48bdf4f7c8003f33a4428bffbef2a85
Youssef <youssefkaramedu@gmail.com>
2023-09-20 23:56:37 -0700

    Add test_user test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,41 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
 from models.user import User
 
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User()
+        self.user.first_name = "3mk Joe"
+        self.user.last_name = "3mk Joe bardo"
+        self.user.email = "guesswhat?3mkjoe@gmail.com"
+        self.user.password = "123456"
 
-class test_User(test_basemodel):
-    """ """
+    def tearDown(self):
+        del self.user
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    def test_email_type(self):
+        self.assertIsInstance(self.user.email, str)
 
-    def test_first_name(self):
-        """ """
-        new = User()
-        self.assertIsInstance(new.first_name, str)
+    def test_password_type(self):
+        self.assertIsInstance(self.user.password, str)
 
-    def test_last_name(self):
-        """ """
-        new = User()
-        self.assertIsInstance(new.last_name, str)
+    def test_first_name_type(self):
+        self.assertIsInstance(self.user.first_name, str)
+
+    def test_last_name_type(self):
+        self.assertIsInstance(self.user.last_name, str)
 
     def test_email(self):
-        """ """
-        new = User()
-        self.assertIsInstance(new.email, str)
+        self.assertEqual(self.user.email, "guesswhat?3mkjoe@gmail.com")
 
     def test_password(self):
-        """ """
-        new = User()
-        self.assertIsInstance(new.password, str)
+        self.assertEqual(self.user.password, "123456")
+
+    def test_first_name(self):
+        self.assertEqual(self.user.first_name, "3mk Joe")
+
+    def test_last_name(self):
+        self.assertEqual(self.user.last_name, "3mk Joe bardo")
+
+if __name__ == "__main__":
+    unittest.main()
+
